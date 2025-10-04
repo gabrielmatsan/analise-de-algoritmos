1b5fa4268954fa71f4c911c4d1a925868115619e
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2025-10-04 10:16:31 -0300

    atualiza e simplifica comentários sobre a complexidade do algoritmo de busca binária, incluindo a contagem de operações e a função de complexidade

--- internal/binary-search.go
+++ internal/binary-search.go
@@ -4,35 +4,27 @@ import "fmt"
 
 func BinarySearch(arr []int, target int) (int, error) {
 
-	// Contagem: 2 operações de atribuição, 1 operação de subtração e 1 operação de tamanho do array = 4 constantes,
-	left, right := 0, len(arr)-1 // C₁ = 4
+	left, right := 0, len(arr)-1 // C₁ = 4 operações
 
-	// Contagem: n operações, log₂(n)
-	for left <= right { // C₂ x log₂(n)
-		// Contagem: 3 operações de atribuição (soma, divisão e atribuição)
-		mid := (left + right) / 2  // C₃ x log₂(n) -> atribuicao, soma e divsao
+	for left <= right { // log(n), pois o espaço é dividido pela metade a cada iteração
 
-		// 1 acesso de array + 1 comparação de target = 2, C₄ x log₂(n)
-		if arr[mid] == target { // C₄ x log₂(n)
-			return mid, nil // C₅ x 1 (apenas um retorno)
+		mid := (left + right) / 2 // 3 * log(n) operações
+
+		if arr[mid] == target { // 2 * log(n) operações
+			return mid, nil // 1 operação
 		}
 
-		// se o elemento do meio for menor que o target, o target está na metade direita
-		// 1 acesso de array + 1 comparação de target = 2, C₆ x log₂(n)
-		if arr[mid] < target {
-			// Contagem: 1 operação de atribuição
-
-			// 1 soma e 1 atribuição = 2, C₇ x log₂(n/2)
-			left = mid + 1
+		if arr[mid] < target { // 2 * log(n) operações
+			left = mid + 1 // 1 * log(n) operações
 		} else {
-			// Contagem: 1 soma e 1 atribuição = 2, C₈ x log₂(n/2)
-			right = mid - 1
+			right = mid - 1 // 1 * log(n) operações
 		}
 	}
-	// Contagem: 1 operação de retorno
-	return -1, fmt.Errorf("target not found") // C₉ x 1
+	return -1, fmt.Errorf("target not found") // 1 operação
 }
-// f(n) = 5 + 10 * log₂(n)
+
+// Função de Complexidade:
+// T(n) = 5 + 10 * log(n)
 
 // Big-O: O(log(n)) - Pior caso
 // Big-Omega: O(1) - Melhor caso
